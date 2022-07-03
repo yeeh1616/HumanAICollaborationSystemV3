30dffbb05931f61d5f7a1cf8f81af0e94da3fed0
Ye <yeeh1616@gmail.com>
2022-07-04 01:07:22 +0200

    v3

--- module1/manage.py
+++ module1/manage.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 from module1 import db
 from module1.models import CoronaNet
@@ -10,11 +10,26 @@ bp_manage = Blueprint('manage', __name__)
 
 @bp_manage.route("/manage", methods=['GET', 'POST'])
 def manage():
-    return render_template('manage.html')
+    policies = CoronaNet.query.all()
+    return render_template('manage.html', policies=policies)
 
 
 @bp_manage.route("/clearall", methods=['GET', 'POST'])
 def clearall():
+    flag = True
+
+    try:
+        num_rows_deleted = db.session.query(CoronaNet).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+        flag = False
+
+    return redirect(url_for('manage.manage'))
+
+
+@bp_manage.route("/clearall0", methods=['GET', 'POST'])
+def clearall0():
     flag = True
 
     try:
